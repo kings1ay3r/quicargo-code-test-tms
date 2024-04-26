a665a124409fa24c4d5e64837720af1a87871a5d
Kevin Mathew Sunny <kevin.mathew@vonnue.com>
2024-04-26 10:17:41 +0530

    fix: update test case descriptions on truck and location services
    Signed-off-by: Kevin Mathew Sunny <kevin.mathew@vonnue.com>

--- src/server/services/trucks/__tests/index.test.ts
+++ src/server/services/trucks/__tests/index.test.ts
@@ -25,7 +25,35 @@ describe('TruckManagementService', () => {
 
       expect(truckManagementService.repository.truck.create).toHaveBeenCalledTimes(1)
     })
+    test('db error on create', async () => {
+      jest.spyOn(truckManagementService.repository.truck, 'create').mockRejectedValue(defaultError)
+      jest
+        .spyOn(truckManagementService.repository.location, 'findByUid')
+        .mockResolvedValue(locationDbResponse)
 
+      await expect(
+        truckManagementService.createTruck(mockContext, createTruckRequest),
+      ).rejects.toThrow(defaultError)
+    })
+    test('db error on find location', async () => {
+      jest
+        .spyOn(truckManagementService.repository.location, 'findByUid')
+        .mockRejectedValue(defaultError)
+
+      await expect(
+        truckManagementService.createTruck(mockContext, createTruckRequest),
+      ).rejects.toThrow(defaultError)
+    })
+  })
+
+  describe('update truck', () => {
+    test('happy path', async () => {
+      jest.spyOn(truckManagementService.repository.truck, 'findByUid').mockResolvedValue(dbResponse)
+      jest.spyOn(truckManagementService.repository.truck, 'update').mockResolvedValue(dbResponse)
+
+      await truckManagementService.updateTruck(mockContext, 'test-truck', updateTruckRequest)
+      expect(truckManagementService.repository.truck.update).toHaveBeenCalledTimes(1)
+    })
     test('db error on update', async () => {
       jest.spyOn(truckManagementService.repository.truck, 'create').mockRejectedValue(defaultError)
       jest
@@ -48,16 +76,6 @@ describe('TruckManagementService', () => {
     })
   })
 
-  describe('update truck', () => {
-    test('happy path', async () => {
-      jest.spyOn(truckManagementService.repository.truck, 'findByUid').mockResolvedValue(dbResponse)
-      jest.spyOn(truckManagementService.repository.truck, 'update').mockResolvedValue(dbResponse)
-
-      await truckManagementService.updateTruck(mockContext, 'test-truck', updateTruckRequest)
-      expect(truckManagementService.repository.truck.update).toHaveBeenCalledTimes(1)
-    })
-  })
-
   describe('delete truck', () => {
     test('happy path', async () => {
       jest.spyOn(truckManagementService.repository.truck, 'delete').mockResolvedValue(dbResponse)
@@ -65,6 +83,58 @@ describe('TruckManagementService', () => {
 
       await truckManagementService.deleteTruck(mockContext, 'test-truck')
       expect(truckManagementService.repository.truck.delete).toHaveBeenCalledTimes(1)
+    })
+    test('db error on delete', async () => {
+      jest.spyOn(truckManagementService.repository.truck, 'delete').mockRejectedValue(defaultError)
+      jest.spyOn(truckManagementService.repository.truck, 'findByUid').mockResolvedValue(dbResponse)
+
+      await expect(truckManagementService.deleteTruck(mockContext, 'test-truck')).rejects.toThrow(
+        defaultError,
+      )
+    })
+    test('db error on find truck', async () => {
+      jest.spyOn(truckManagementService.repository.truck, 'delete').mockResolvedValue(dbResponse)
+      jest
+        .spyOn(truckManagementService.repository.truck, 'findByUid')
+        .mockRejectedValue(defaultError)
+
+      await expect(truckManagementService.deleteTruck(mockContext, 'test-truck')).rejects.toThrow(
+        defaultError,
+      )
+    })
+  })
+  describe('get truck', () => {
+    test('happy path', async () => {
+      const { id, locationId, ...expectedResponse } = dbResponse
+      jest.spyOn(truckManagementService.repository.truck, 'findByUid').mockResolvedValue(dbResponse)
+
+      const truck = await truckManagementService.getTruck(mockContext, 'test-truck')
+
+      expect(truck.data).toEqual([expectedResponse])
+    })
+    test('db error on find truck', async () => {
+      jest
+        .spyOn(truckManagementService.repository.truck, 'findByUid')
+        .mockRejectedValue(defaultError)
+
+      await expect(truckManagementService.getTruck(mockContext, 'test-truck')).rejects.toThrow(
+        defaultError,
+      )
+    })
+  })
+  describe('get trucks', () => {
+    test('happy path', async () => {
+      const { id, locationId, ...expectedResponse } = dbResponse
+      jest.spyOn(truckManagementService.repository.truck, 'find').mockResolvedValue([dbResponse])
+
+      const trucks = await truckManagementService.getTrucks(mockContext)
+
+      expect(trucks.data).toEqual([expectedResponse])
+    })
+    test('db error on find trucks', async () => {
+      jest.spyOn(truckManagementService.repository.truck, 'find').mockRejectedValue(defaultError)
+
+      await expect(truckManagementService.getTrucks(mockContext)).rejects.toThrow(defaultError)
     })
   })
 })
